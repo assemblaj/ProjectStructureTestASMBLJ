8ca88fd2d5dc213af2897e93c64134b9b6f21954
arheard <arheard0@gmail.com>
2019-03-21 15:46:15 -0400

    Adds files

---
+++ cmd/package_test/main.go
@@ -0,0 +1,10 @@
+package main
+
+//mport {
+// import github links to packages being used in main, here
+// }
+
+func main() {
+	printA()
+	printB()
+}
---
+++ pkg/a/a_test.go
@@ -0,0 +1,14 @@
+package a 
+
+import {
+	"testing"
+	"b"
+}
+
+func TestPrintA(t *t.Testing) {
+	PrintA() 
+}
+
+func TestPrintB(t *t.Testing) {
+	b.PrintA() 
+}
